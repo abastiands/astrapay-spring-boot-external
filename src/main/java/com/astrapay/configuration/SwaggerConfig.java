285032ea67ac4914a52c396dbe8c83bd11502cdc
Abastian Dwi Saputra <abastiands@gmail.com>
2025-09-07 20:10:50 +0700

    simplenoteap: adding swagger config and pom.xml

---
+++ src/main/java/com/astrapay/configuration/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.astrapay.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Simple Note App API Documentation")
+                .description("Dokumentasi aplikasi Simple Note App API By Abastian Dwi Saputra pada tahun 2025")
+                .version("1.0.0")
+                .build();
+    }
+}
